410b167ec81ad41c7d770358db48093b720b44b9
Rodrigo Ivashita <rodrigo.ivashita@gmail.com>
2020-12-29 20:17:45 -0300

    Chess layer and printing the board

---
+++ src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPrices());
+		
+
+	}
+
+}
